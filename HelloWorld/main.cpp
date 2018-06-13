8040bc85181e12e442fbd2738a16219652f3d30b
logh <kircheis@imac.local>
2018-06-13 23:31:44 +0300

    Initial Commit

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HelloWorld
+//
+//  Created by logh on 6/13/18.
+//  Copyright © 2018 logh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
