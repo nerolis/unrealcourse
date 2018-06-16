fdf98278d71a3b9a29f5a476852862202b4a21ab
nerolis <yakumoft@gmail.com>
2018-06-16 14:36:17 +0300

    ep 28, move class to header, delete namespaces in main, class methods

--- HelloWorld/FBullCowGame.cpp
+++ HelloWorld/FBullCowGame.cpp
@@ -1,23 +1,24 @@
-	//
-//  FBullCowGame.cpp
-//  HelloWorld
-//
-//  Created by logh on 6/15/18.
-//  Copyright © 2018 logh. All rights reserved.
-//
-
 #include "FBullCowGame.hpp"
 
-class FBullCowGame {
-public:
-    void Reset();
-    int GetMaxTries();
-    int GetCurrentTry;
-    bool IsGameWon();
-    bool CheckGuessValidity();
+void FBullCowGame::Reset() { 
+    return;
+}
 
-private:
-    int MyCurrentTry;
-    int MyMaxTries;
-    bool IsIsogram();
-};
+bool FBullCowGame::IsGameWon() {
+    return 0;
+}
+
+bool FBullCowGame::CheckGuessValidity(std::string) { 
+    return 0;
+}
+
+
+int FBullCowGame::GetMaxTries() { 
+    return 0;
+}
+
+
+int FBullCowGame::GetCurrentTry() { 
+    return 0;
+}
+
--- HelloWorld/main.cpp
+++ HelloWorld/main.cpp
@@ -1,11 +1,10 @@
 #include <iostream>
-
-using namespace std;
+#include <string>
 
 // Func Proto
 void GameIntro();
 void PlayGame();
-string GetGuess();
+std::string GetGuess();
 bool PlayAgain();
 
 int main()
@@ -20,11 +19,11 @@ int main()
 }
 
 // Get a Guess from the Player
-string GetGuess()
+std::string GetGuess()
 {
-    cout << "Enter your guess: ";
-    string Guess = "";
-    getline(cin, Guess);
+    std::cout << "Enter your guess: ";
+    std::string Guess = "";
+    getline(std::cin, Guess);
     return Guess;
 }
 
@@ -33,10 +32,10 @@ string GetGuess()
 void GameIntro()
 {
     constexpr int WORLD_LENGTH = 5;
-    cout << "Welcome to Bulls and Cows, a fun game." << endl;
-    cout << "Can you guess the " << WORLD_LENGTH;
-    cout << "letter isogram i'm thinking of?" << endl;
-    cout << endl;
+    std::cout << "Welcome to Bulls and Cows, a fun game." << std::endl;
+    std::cout << "Can you guess the " << WORLD_LENGTH;
+    std::cout << "letter isogram i'm thinking of?" << std::endl;
+    std::cout << std::endl;
     return;
 }
 
@@ -45,27 +44,27 @@ void PlayGame()
     constexpr int NUMBER_OF_TURNS = 5;
     for (int count = 1; count <= NUMBER_OF_TURNS; count++)
     {
-        string Guess = GetGuess();
-        cout << "Your guess was: " << Guess << endl;
-        cout << endl;
+        std::string Guess = GetGuess();
+        std::cout << "Your guess was: " << Guess << std::endl;
+        std::cout << std::endl;
     }
 }
 
 bool PlayAgain()
 {
-    cout << "Do you want to play again? Y/N?" << endl;
-    string Choice = "";
-    getline(cin, Choice);
+    std::cout << "Do you want to play again? Y/N?" << std::endl;
+    std::string Choice = "";
+    getline(std:: cin, Choice);
 
 
 
-    cout << "Player choice: " << (Choice[0] == 'y');
-    cout << endl;
+    std::cout << "Player choice: " << (Choice[0] == 'y');
+    std::cout << std::endl;
 
     if (Choice[0] == 'y' || Choice[0] == 'Y')
-        cout << "Restarting the game..." << endl;
+        std::cout << "Restarting the game..." << std::endl;
     else
-        cout << "Player choiced finish the game" << endl;
+        std::cout << "Player choiced finish the game" << std::endl;
 
     return (Choice[0] == 'y' || (Choice[0] == 'Y'));
 }
