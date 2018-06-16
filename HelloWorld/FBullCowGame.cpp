c5b2e1195fec7e9bb3364bde6f9f2a5a970f51ed
nerolis <yakumoft@gmail.com>
2018-06-16 16:58:52 +0300

    ep 35, struct

--- HelloWorld/FBullCowGame.cpp
+++ HelloWorld/FBullCowGame.cpp
@@ -6,21 +6,37 @@ FBullCowGame::FBullCowGame() { Reset(); }
 int32 FBullCowGame::GetMaxTries() const { return MyMaxTries; }
 int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
 
-void FBullCowGame::Reset() {
+void FBullCowGame::Reset()
+{
     constexpr int32 MAX_TRIES = 8;
     MyMaxTries = MAX_TRIES;
+
+    const FString HIDDEN_WORD = "planet";
+    MyHiddenWord = HIDDEN_WORD;
 
     MyCurrentTry = 1;
     return;
 }
 
-bool FBullCowGame::IsGameWon() const {
+bool FBullCowGame::IsGameWon() const
+{
     return 0;
 }
 
-bool FBullCowGame::CheckGuessValidity(std::string) { 
+bool FBullCowGame::CheckGuessValidity(std::string)
+{
     return 0;
 }
+
+// Receives a VALID guess, increments and returns count;
+BullCowCount FBullCowGame::SubmitGuess(FString)
+{
+    MyCurrentTry++;
+    BullCowCount BullCowCount;
+
+    return BullCowCount;
+};
+
 
 
 
--- HelloWorld/FBullCowGame.hpp
+++ HelloWorld/FBullCowGame.hpp
@@ -2,20 +2,28 @@
 using int32 = int;
 using FString = std::string;
 
+struct BullCowCount
+{
+    int32 Bulls = 0;
+    int32 Cows = 0;
+};
+
 class FBullCowGame {
 public:
     FBullCowGame(); // Constructor
 
-    int GetMaxTries() const;
-    int GetCurrentTry() const;
+    int32 GetMaxTries() const;
+    int32 GetCurrentTry() const;
     bool IsGameWon() const;
 
     void Reset();
     bool CheckGuessValidity(FString);
 
+    BullCowCount SubmitGuess(FString);
+
 private:
     // Init in constructor;
     int32 MyCurrentTry;
     int32 MyMaxTries;
-    bool IsIsogram();
+    FString MyHiddenWord;
 };
